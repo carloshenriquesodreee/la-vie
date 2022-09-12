487789414919abc455500441d954182230678adc
Jessica Arf <jessicalealarf8@gmail.com>
2022-09-12 18:53:10 -0300

    conexão do banco de dados criada

---
+++ src/database/index.js
@@ -0,0 +1,35 @@
+const Sequelize = require("sequelize");
+
+const DB_NAME = "la_vie";
+const DB_USER = "root";
+const DB_PASS = "";
+const DB_CONFIG = {
+  dialect: "mysql",
+  host: "localhost",
+  port: 3306,
+};
+
+let db = {};
+
+try {
+  db = new Sequelize(DB_NAME, DB_USER, DB_PASS, DB_CONFIG);
+} catch (error) {
+  console.log(error);
+  console.error("Error ao tentar uma conexão com banco dados");
+}
+
+async function hasConection() {
+  try {
+    await db.authenticate();
+    console.log("Banco de dados conectado!");
+  } catch (error) {
+    console.log(error);
+    console.error("Erro ao tentar se conectar ao banco de dados1");
+  }
+}
+
+Object.assign(db, {
+  hasConection,
+});
+
+module.exports = db;
