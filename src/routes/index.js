665337e5e40d106031997e52e6462d741c0e14aa
Jessica Arf <jessicalealarf8@gmail.com>
2022-09-16 00:23:23 -0300

    modificando rota pacientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,11 @@ const pacienteController = require("../controller/pacientesController")
 const authController = require("../controller/authController");
 const psicologoCreateValidation = require("../validations/psicologos/create");
 const authLoginValidation = require("../validations/auth/login");
-const atendimentoController = require("../controller/atendimentoController")
-const auth = require("../middlewares/auth")
+const atendimentoController = require("../controller/atendimentoController");
+const auth = require("../middlewares/auth");
 const routes = express.Router();
 
-routes.get("/paciente/:id", pacienteController.listOne);
+routes.get("/pacientes/:id", pacienteController.listOne);
 routes.put("/pacientes/:id", pacienteController.updatePacientes);
 routes.get("/pacientes", pacienteController.listarPaciente);
 routes.post("/pacientes", pacienteController.registerPacientes);
